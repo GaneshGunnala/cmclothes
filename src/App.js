96a2c960736edfe7d3a3bd5bfc9dd2fe2b17c721
Ganesh Gunnala <gganesh@mirabeltechnologies.com>
2023-04-11 13:33:35 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import logo from './logo.svg';
+import routes from "./routes";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter >
+						<Routes>
+							{routes.map((route, index) => {debugger;
+              return (
+								<Route key={index} exact path={route.path} element={route.component} />
+							)})}
+						</Routes>
+					</BrowserRouter>
   );
 }
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState, useReducer } from "react";
+const Home = () => {
+return(
+    <div>
+        Hello world
+    </div>
+);
+}
+
+export default Home;
